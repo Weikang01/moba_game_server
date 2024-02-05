414b2db429075411ebf6eb317a5bbb40631cb7d7
Weikang01 <lwkdota@qq.com>
2024-02-05 00:12:03 -0500

    mysql connection

---
+++ database/mysql_wrapper.h
@@ -0,0 +1,19 @@
+#ifndef __MYSQL_WRAPPER_H__
+#define __MYSQL_WRAPPER_H__
+
+#include <vector>
+#include <string>
+
+class mysql_wrapper
+{
+public:
+	static void connect(const char* host, int port, const char* user, const char* pass, const char* db_name, void (*open_cb)(const char* err, void* context));
+
+	static void query(void* context, const char* sql, void (*query_cb)(const char* err, std::vector<std::vector<std::string>>* result));
+
+	static void close(void* context);
+};
+
+
+
+#endif
